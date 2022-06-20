469f75976679ac9241c3e0131c7fed37da75ed3e
Cristi Nica <nicacristi20@gmail.com>
2022-06-20 19:54:24 +0300

    Update message

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Salut, imi place React!
         </a>
       </header>
     </div>
